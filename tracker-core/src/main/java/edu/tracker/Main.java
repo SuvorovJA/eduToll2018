a25003381d4a1c337d6195c35f5ab6c706a61450
SuvorovJA <riskiroy@gmail.com>
2018-03-05 00:23:09 +0700

    копия всех src toll-2017-gradle включение fatJar. падает.

---
+++ tracker-core/src/main/java/edu/tracker/Main.java
@@ -0,0 +1,19 @@
+package edu.tracker;
+
+import edu.dto.PointDTO;
+
+/**
+ * Created by jdev on 07.03.2017.
+ * Copy by sua on 05.03.2018
+ */
+public class Main {
+    public static void main(String... args) throws Exception {
+        for (int i=0; i<5; i++) {
+            System.out.println("Main.main say Hello!!!!");
+            PointDTO point = new PointDTO();
+            point.setLat(45);
+            System.out.println(point.toJson());
+            Thread.sleep(1000);
+        }
+    }
+}
