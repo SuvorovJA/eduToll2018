99ba30b864f084874bf284e2fd5fa6fb12fd2a1b
SuvorovJA <riskiroy@gmail.com>
2018-03-20 16:58:03 +0700

    конфиг mvc (9 + /)

---
+++ server-ui/src/main/java/edu/server/ui/config/MvcConfig.java
@@ -0,0 +1,24 @@
+package edu.server.ui.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/routes").setViewName("routes");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/error").setViewName("error");
+        registry.addViewController("/payments").setViewName("payments");
+        registry.addViewController("/img").setViewName("img/index"); //?
+        registry.addViewController("/css").setViewName("css/index"); //?
+        registry.addViewController("/registerManager").setViewName("registerManager");
+        registry.addViewController("/registerClient").setViewName("registerClient");
+    }
+
+}
