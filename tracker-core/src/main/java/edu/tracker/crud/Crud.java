4c7c5b3a0accada020577d5c675936aafe92f6e7
SuvorovJA <riskiroy@gmail.com>
2018-04-11 02:00:42 +0700

    tracker-core: CrudIntegrationTest, +Lombok, copypaste Crud & PointDTOEntity

--- tracker-core/src/main/java/edu/tracker/crud/Crud.java
+++ tracker-core/src/main/java/edu/tracker/crud/Crud.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+//import lombok.extern.slf4j.Slf4j;
+
+//@Slf4j
 public class Crud {
 
     private static final Logger log = LoggerFactory.getLogger(Crud.class);
@@ -19,9 +22,41 @@ public class Crud {
     @Autowired
     PointDTORepository pointDTORepository;
 
+    public void delete(PointDTOEntity point) {
+        pointDTORepository.delete(point);
+    }
 
-    // other methods no need this
+    public void update(PointDTOEntity point, PointDTO pointObj) {
+        point.setAutoId(pointObj.getAutoId());
+        point.setLat(pointObj.getLat());
+        point.setLon(pointObj.getLon());
+        point.setTime(pointObj.getTime());
+        pointDTORepository.save(point);
+    }
 
+    public void update(PointDTOEntity point) {
+        pointDTORepository.save(point);
+    }
+
+    public PointDTOEntity read(int id){
+        return pointDTORepository.findOne(id);
+    }
+
+    // можно зашедулить для мониторинга содержимого таблицы если не жалко лога
+    public void read() {
+        // будет мегадлинный-же вывод
+        all = (List<PointDTOEntity>) pointDTORepository.findAll();
+        if (all.size() == 0) {
+            log.info("NO RECORDS");
+        } else {
+            all.stream().forEach(point -> log.info(point.toString()));
+        }
+    }
+
+    //в логи прилетает (при включенном дебаге гибернейта)
+    //      2018-04-09 20:54:00.261 DEBUG 12711 --- [nio-8090-exec-8] org.hibernate.SQL
+    //                  : insert into points (id, autoid, lat, lon, time) values (null, ?, ?, ?, ?)
+    //однако эхо из этого метода и записи в БД нормальные                        ~~~~~~~~~~~~~~~~~~~
     public PointDTOEntity create(PointDTO pointObj) {
         PointDTOEntity point = new PointDTOEntity();
         point.setAutoId(pointObj.getAutoId());
--- tracker-core/src/main/java/edu/tracker/repository/PointDTOEntity.java
+++ tracker-core/src/main/java/edu/tracker/repository/PointDTOEntity.java
@@ -1,10 +1,16 @@
 package edu.tracker.repository;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 import static javax.persistence.GenerationType.AUTO;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "points")
 public class PointDTOEntity {
@@ -25,59 +31,4 @@ public class PointDTOEntity {
 
     @Column(name = "TIME")
     long time;
-
-    //
-
-    @Override
-    public String toString() {
-        return "PointDTOEntity{" +
-                "id=" + id +
-                ", lat=" + lat +
-                ", lon=" + lon +
-                ", autoId='" + autoId + '\'' +
-                ", time=" + time +
-                '}';
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public double getLat() {
-        return lat;
-    }
-
-    public void setLat(double lat) {
-        this.lat = lat;
-    }
-
-    public double getLon() {
-        return lon;
-    }
-
-    public void setLon(double lon) {
-        this.lon = lon;
-    }
-
-    public String getAutoId() {
-        return autoId;
-    }
-
-    public void setAutoId(String autoId) {
-        this.autoId = autoId;
-    }
-
-    public long getTime() {
-        return time;
-    }
-
-    public void setTime(long time) {
-        this.time = time;
-    }
-
-
 }
