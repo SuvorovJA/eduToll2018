d3eef3b997556d3721f43b8f663b0237a19c2cd8
SuvorovJA <riskiroy@gmail.com>
2018-04-11 00:44:00 +0700

    server-core: переход на idea 2018.1CE + lombok plugin + lombok gradle dep. server-core:PointDTOEntity сделан с аннотациями lombok

--- server-core/src/main/java/edu/server/repository/PointDTOEntity.java
+++ server-core/src/main/java/edu/server/repository/PointDTOEntity.java
@@ -1,32 +1,20 @@
 package edu.server.repository;
 
-
-
-//import lombok.Getter;                         //
-//import lombok.NoArgsConstructor;              //
-//import lombok.Setter;                         //
-//import lombok.ToString;                       // lombok no work
-//import lombok.extern.log4j.Log4j;             //
-//import lombok.extern.slf4j.Slf4j;             //
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Table;
-
-// Err: No identifier specified for entity: edu.server.repository.PointDTOEntity
-//import org.springframework.data.annotation.Id;  conflict with
 import javax.persistence.Id;
 
 import static javax.persistence.GenerationType.AUTO;
 
-
-//@Slf4j                //
-//@Getter               //
-//@Setter               // lombok no work
-//@ToString             //
-//@NoArgsConstructor    //
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "points")
 public class PointDTOEntity {
@@ -35,7 +23,6 @@ public class PointDTOEntity {
     @GeneratedValue(strategy = AUTO)
     @Column(name = "ID")
     int id;
-
 
     @Column(name = "LAT")
     double lat;
@@ -48,59 +35,4 @@ public class PointDTOEntity {
 
     @Column(name = "TIME")
     long time;
-
-    //
-
-    @Override
-    public String toString() {
-        return "PointDTOEntity{" +
-                "id=" + id +
-                ", lat=" + lat +
-                ", lon=" + lon +
-                ", autoId='" + autoId + '\'' +
-                ", time=" + time +
-                '}';
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public double getLat() {
-        return lat;
-    }
-
-    public void setLat(double lat) {
-        this.lat = lat;
-    }
-
-    public double getLon() {
-        return lon;
-    }
-
-    public void setLon(double lon) {
-        this.lon = lon;
-    }
-
-    public String getAutoId() {
-        return autoId;
-    }
-
-    public void setAutoId(String autoId) {
-        this.autoId = autoId;
-    }
-
-    public long getTime() {
-        return time;
-    }
-
-    public void setTime(long time) {
-        this.time = time;
-    }
-
-
 }
